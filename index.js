56952ddacb8fbc2af93b278f013dd304f30367b8
SiechiOficial <siechi2012alt@gmail.com>
2024-08-22 04:16:31 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function mainSaveDate(){
+    getData()
+    checkValues()
+    sendToDatabase()
+}
+
+// função que pega todos os dados 
+function getData(){
+    console.log("Pegando dados do usuario ")
+    if (1 < 4){
+        console.log("numero encontrado")
+    }
+}
+
+function checkValues(){
+    console.log("validando dados")
+}
+
+function sendToDatabase(){
+    console.log("cadastrando dados")
+}
